6f6c138636e71577a856bbef75ee40d5fe042e0a
Laym <65513977+LaymGlitched@users.noreply.github.com>
2026-09-04 18:14:30 +0100

    Initial commit

---
+++ Core.cs
@@ -0,0 +1,15 @@
+﻿using MelonLoader;
+
+[assembly: MelonInfo(typeof(GetToModLib.Core), "Get To Mod Lib", "1.0.0", "itslaymxd", null)]
+[assembly: MelonGame("Isto", "Get To Work")]
+
+namespace GetToModLib
+{
+    public class Core : MelonMod
+    {
+        public override void OnInitializeMelon()
+        {
+            LoggerInstance.Msg("Initialized.");
+        }
+    }
+}
